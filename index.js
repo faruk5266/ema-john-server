a19748650bb802fc3ebb09287937b83a75bbd2dd
faruk5266 <farukhussain5266@gmail.com>
2020-10-04 12:00:18 +0600

    root add

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ client.connect(err => {
 
   })
 
+  app.get('/', (req, res) => {
+    res.send("hello db ! now it's working")
+  })
+
   app.get('/products', (req, res) => {
     productsCollection.find({})
       .toArray((err, documents) => {
